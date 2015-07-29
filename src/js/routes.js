d23c1c3045e4a7c7a8631ad5fa4bac4164cd1df3
Kocsen <kocsenc@gmail.com>
2015-07-28 20:13:07 -0700

    general reformat

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,41 +1,41 @@
 'use strict';
 
 angular.module('mission-control').config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-        // For unmatched routes
-        $urlRouterProvider.otherwise('/');
+  function ($stateProvider, $urlRouterProvider) {
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
 
-        // Application routes
-        $stateProvider
-            .state('index', {
-                url: '/',
-                templateUrl: 'templates/overview.html',
-                controller: 'OverviewCtrl'
-            })
-            .state('agenda', {
-                url: '/agenda',
-                templateUrl: 'templates/agenda.html',
-                controller: 'AgendaCtrl'
-            })
-            .state('events', {
-                url: '/events',
-                templateUrl: 'templates/events.html',
-                controller: 'EventsCtrl'
-            })
-            .state('members', {
-                url: '/members',
-                templateUrl: 'templates/members.html',
-                controller: 'MembersCtrl'
-            })
-            .state('sponsors', {
-                url: '/sponsors',
-                templateUrl: 'templates/sponsors.html',
-                controller: 'SponsorsCtrl'
-            })
-            .state('tasks', {
-                url: '/tasks',
-                templateUrl: 'templates/tasks.html',
-                controller: 'TasksCtrl'
-            });
-    }
+    // Application routes
+    $stateProvider
+      .state('index', {
+        url: '/',
+        templateUrl: 'templates/overview.html',
+        controller: 'OverviewCtrl'
+      })
+      .state('agenda', {
+        url: '/agenda',
+        templateUrl: 'templates/agenda.html',
+        controller: 'AgendaCtrl'
+      })
+      .state('events', {
+        url: '/events',
+        templateUrl: 'templates/events.html',
+        controller: 'EventsCtrl'
+      })
+      .state('members', {
+        url: '/members',
+        templateUrl: 'templates/members.html',
+        controller: 'MembersCtrl'
+      })
+      .state('sponsors', {
+        url: '/sponsors',
+        templateUrl: 'templates/sponsors.html',
+        controller: 'SponsorsCtrl'
+      })
+      .state('tasks', {
+        url: '/tasks',
+        templateUrl: 'templates/tasks.html',
+        controller: 'TasksCtrl'
+      });
+  }
 ]);
