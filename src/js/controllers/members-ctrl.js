2d714ba4210fead3fee67b29ad16cda003f22f97
Kocsen <kocsenc@gmail.com>
2015-07-29 21:08:13 -0700

    -memberpage flushed

--- src/js/controllers/members-ctrl.js
+++ src/js/controllers/members-ctrl.js
@@ -6,8 +6,24 @@ angular.module('mission-control')
   .controller('MembersCtrl', ['$scope', 'Members', MembersCtrl]);
 
 function MembersCtrl($scope, Members) {
-  Members.get(function (responseData) {
-    $scope.members = responseData;
-    console.log($scope.members.data);
-  })
+  getMembers();
+
+  $scope.pagination = {
+    per_page: null,
+    current_page: null,
+    last_page: null,
+    next_page_url: null,
+    prev_page_url: null,
+    from: null,
+    to: null
+  };
+
+  function getMembers() {
+    Members.get(function (responseData) {
+      $scope.members = responseData.data;
+      $scope.totalMembers = responseData.total;
+    })
+  }
+
+
 }
