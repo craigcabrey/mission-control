2b740a55d55ba37b5fb699374ae0a56c0ea56cc0
Kocsen <kocsenc@gmail.com>
2015-07-30 17:31:43 -0700

    new chartjs

--- src/js/controllers/overview-ctrl.js
+++ src/js/controllers/overview-ctrl.js
@@ -37,4 +37,58 @@ function OverviewCtrl($scope, Members, Events, Sponsors) {
     $scope.sponsors.total = data.length;
   });
 
+  angular.element(document).ready(function () {
+    var data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Members",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [10, 15, 12, 20, 22, 43, 57, 57]
+        },
+        {
+          label: "Memberships",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [8, 14, 11, 15, 17, 40, 45, 45]
+        }
+      ]
+    };
+
+    var memberLineChartConfig = {
+      // Grid Lines
+      scaleShowGridLines: false,
+      scaleGridLineColor: "rgba(0,0,0,.05)",
+
+      //Whether the line is curved between points
+      bezierCurve: true,
+      bezierCurveTension: 0.3,
+
+      // To show a dot for each point
+      pointDot: true,
+      pointDotRadius: 3,
+      pointDotStrokeWidth: 1,
+
+      // Line/Stroke configuration
+      datasetStroke: true,
+      datasetStrokeWidth: 2,
+
+      //Boolean - Whether to fill the dataset with a colour
+      datasetFill: false
+
+    };
+
+
+    var ctx = document.getElementById("memberChart").getContext("2d");
+    var memberLineChart = new Chart(ctx).Line(data, memberLineChartConfig);
+  });
 }
